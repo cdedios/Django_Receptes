95e80265fe8b39bb66cba31dcca5f5e41a30615f
Carlos <carlos@carlos-U31F.(none)>
2013-03-27 19:34:07 +0100

    canvis a models.py i views.py per a fer el User

--- Receptes/iReceptes/models.py
+++ Receptes/iReceptes/models.py
@@ -1,13 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Ingredient(models.Model):
-  name = models.TextField(max_length=100)
+  name = models.CharField(max_length=40)
   description = models.TextField(max_length=1000)
+  def __unicode__(self):
+    return self.name
 
 class Cuiner(models.Model):
   name = models.TextField(max_length=100)
   
 class Recepta(models.Model):
   name = models.TextField(max_length=100)
+  description = models.TextField(max_length=1000)
+  ingredients = models.ForeingKey(Ingredtient)
+  user = models.ForeingKey(User)
+  def __unicode__(self):
+    return self.ingredients.name+ " - "+self.description
--- Receptes/iReceptes/views.py
+++ Receptes/iReceptes/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import Context
 from django.template.loader import get_template
+from django.contrib.auth.models import User
 
 def mainpage(request):
   template = get_template('mainpage.html')
@@ -9,5 +10,19 @@ def mainpage(request):
     'pagetitle': 'Welcome to the Receptes  app',
     'contentbody': 'Managing non legal funding since 2013'
   })
+  output = template.render(variables)
+  return HttpResponse(output)
+
+def userpage(request, username):
+  try:
+    user = User.objects.get(username=username)
+  except:
+    raise Http404('User not found.')
+  ingredients = user.sobre_set.all()
+  template = get_template('userpage.html')
+  variables = Context({
+    'username': username,
+    'ingredients': ingredients
+    })
   output = template.render(variables)
   return HttpResponse(output)
