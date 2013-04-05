e5fb5b81f9e3b4b95702d15887dd2f9ef01b4731
Carlos <carlos@carlos-U31F.(none)>
2013-04-05 10:45:57 +0200

    Llista el nom de les receptes actuals. Done!!

--- Receptes/iReceptes/views.py
+++ Receptes/iReceptes/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, Http404
 from django.template import Context
 from django.template.loader import get_template
 from django.contrib.auth.models import User
+from iReceptes.models import Recepta
 
 def mainpage(request):
   template = get_template('mainpage.html')
@@ -16,7 +17,8 @@ def mainpage(request):
 def receptes_list(request):
   template = get_template('receptes.html')
   variables = Context({
-      'titlehead': 'Llista de totes les Receptes',      
+      'titlehead': 'Llista de totes les Receptes',
+      'Receptas' : Recepta.objects.all      
     })
   output = template.render(variables)
   return HttpResponse(output)
